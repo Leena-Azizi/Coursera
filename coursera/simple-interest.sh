dabc8d930c53884d113f9f398a88bd1da2ed37a8
Leena-Azizi <leenamuhannad03@gmail.com>
2024-12-26 08:56:52 +0300

    Update simple-interest.sh

--- coursera/simple-interest.sh
+++ coursera/simple-interest.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+# Simple Interest Calculator
+echo "Enter Principal Amount:"
+read principal
+echo "Enter Rate of Interest:"
+read rate
+echo "Enter Time (in years):"
+read time
+simple_interest=$(echo "$principal * $rate * $time / 100" | bc -l)
+echo "Simple Interest is: $simple_interest"
 
